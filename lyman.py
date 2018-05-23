e5e04a7811a8b1c57388389053526da691ecbf99
Paulo Montero Camacho <monterocamacho.1@osu.edu>
2018-05-23 09:37:45 -0400

    new optimized version

--- lyman.py
+++ lyman.py
@@ -37,6 +37,7 @@ def Dratio(z_obs,z):
 
 print Dratio(1, 21)
 
+#*************************************************************************************************************************************
 #load 21cmfast realization 
 #table = np.loadtxt('power_default1')
 #table = np.loadtxt('power_default2')
@@ -44,8 +45,12 @@ print Dratio(1, 21)
 #table = np.loadtxt('power_default4')
 #table = np.loadtxt('power_default5')
 #table = np.loadtxt('power_default6')
-table = np.loadtxt('power_default7')
+#table = np.loadtxt('power_default7')
 #table = np.loadtxt('power_default8')
+table = np.loadtxt('power_modelA1')
+
+
+
 
 p_mxh   = table[:,2]
 print p_mxh
@@ -75,7 +80,7 @@ for o in range(0,84):
 	deltaplot_array[o] = crosspower(z_array[o], 0.1)
 	o = o + 1	
 
-np.savetxt('plotdeltamxh_default7.dat', np.transpose([z_array, deltaplot_array]), fmt='%e', delimiter=' ')
+np.savetxt('plotdeltamxh_modelA1.dat', np.transpose([z_array, deltaplot_array]), fmt='%e', delimiter=' ')
 
 
 
@@ -84,7 +89,7 @@ np.savetxt('plotdeltamxh_default7.dat', np.transpose([z_array, deltaplot_array])
 
 
 
-
+#**************************************************************************************************************************
 #PLOTTING STARTED
 #important: z1 = 2.0, z2 =2.5, z3=3.0, z4=3.5, z5=4.0
 #time to deal with the bias representative, do a piecewise linear guy.
@@ -169,6 +174,13 @@ plt.show()
 
 # PLOTTING ENDED
 
+
+
+
+
+
+
+#*******************************************************************************************************************************************
 #integral implementation with constant  psi derivative with  respect to z
 #dpsi = 0.0715296
 delta_z = 0.14
@@ -279,6 +291,16 @@ crosspower_final = interp2d(zobs_array, k_array, crosspmxh_array, kind='cubic')
 print 'The crosspower spectrum at observer redshift 2.2 and wavenumber 0.9999 is %f\n' % crosspower_final(2.2, 0.9999)
 
 
+
+poraca = crosspower_final(2.5,k_test)*(2.0*np.pi**2)/(1.0*k_test**3)
+tomala = crosspower_z2(redshift_test,k_test)*(2.0*np.pi**2)/(1.0*k_test**3)
+print 'HERE HERE HERE The crosspower spectrum at zobs =2.5 with wave number 0.0997 is %f or more precissely %f \n' % (poraca,tomala)
+
+
+
+
+
+
 #************************************************************************************************************************
 #this is where  matter matter starts
 #comparison with matter power spectrum at observation redshift
@@ -365,6 +387,8 @@ def matterpower_z5(k):
 def comparison(cross,auto):
 	return (-1*cross/auto)*200
 
+#note that the minus sign comes from neutral to ionized change, and note that it is not included in the crosspower function
+
 final_z2 = comparison(crosspower_z2(redshift_test, k_test),matterpower_z2(k_test))
 
 final_z1 = comparison(crosspower_z1(redshift_test, k_test),matterpower_z1(k_test))
@@ -438,24 +462,41 @@ print 'Comparison effect with bias ratio at zobs = 4.0 and k = %f is %e \n' % (k
 
 #here I want to plot the final cross-power as a function of k, for a given zobs, in this case zobs = 2.5, then z1 = zobs= 2.0
 m=0
-deltamm_array = np.zeros(16)
-crossplot_array = np.zeros(16)
+deltammz1_array = np.zeros(16)
+deltammz2_array = np.zeros(16)
+deltammz3_array = np.zeros(16)
+deltammz4_array = np.zeros(16)
+deltammz5_array = np.zeros(16)
+crossplotz1_array = np.zeros(16)
+crossplotz2_array = np.zeros(16)
+crossplotz3_array = np.zeros(16)
+crossplotz4_array = np.zeros(16)
+crossplotz5_array = np.zeros(16)
 for m in range(0,16):
-	#crossplot_array[m] = crosspower_z2(redshift_test, k_array[m])
-	#crossplot_array[m] = crosspower_z1(redshift_test, k_array[m])
-	#crossplot_array[m] = crosspower_z3(redshift_test, k_array[m])
-	#crossplot_array[m] = crosspower_z4(redshift_test, k_array[m])	
-	crossplot_array[m] = crosspower_z5(redshift_test, k_array[m])
-	#deltamm_array[m] = matterpower_z2(k_array[m]) 
-	#deltamm_array[m] = matterpower_z1(k_array[m])
-	#deltamm_array[m] = matterpower_z3(k_array[m])
-	#deltamm_array[m] = matterpower_z4(k_array[m])
-	deltamm_array[m] = matterpower_z5(k_array[m])
+	crossplotz2_array[m] = crosspower_z2(redshift_test, k_array[m])
+	crossplotz1_array[m] = crosspower_z1(redshift_test, k_array[m])
+	crossplotz3_array[m] = crosspower_z3(redshift_test, k_array[m])
+	crossplotz4_array[m] = crosspower_z4(redshift_test, k_array[m])	
+	crossplotz5_array[m] = crosspower_z5(redshift_test, k_array[m])
+	deltammz2_array[m] = matterpower_z2(k_array[m]) 
+	deltammz1_array[m] = matterpower_z1(k_array[m])
+	deltammz3_array[m] = matterpower_z3(k_array[m])
+	deltammz4_array[m] = matterpower_z4(k_array[m])
+	deltammz5_array[m] = matterpower_z5(k_array[m])
 	m = m + 1	
 
-np.savetxt('plotprueba_default7z5.dat', np.transpose([k_array, crossplot_array, deltamm_array]), fmt='%e', delimiter=' ')
-
+np.savetxt('plotprueba_modelA1z1.dat', np.transpose([k_array, crossplotz1_array, deltammz1_array]), fmt='%e', delimiter=' ')
+np.savetxt('plotprueba_modelA1z2.dat', np.transpose([k_array, crossplotz2_array, deltammz2_array]), fmt='%e', delimiter=' ')
+np.savetxt('plotprueba_modelA1z3.dat', np.transpose([k_array, crossplotz3_array, deltammz3_array]), fmt='%e', delimiter=' ')
+np.savetxt('plotprueba_modelA1z4.dat', np.transpose([k_array, crossplotz4_array, deltammz4_array]), fmt='%e', delimiter=' ')
+np.savetxt('plotprueba_modelA1z5.dat', np.transpose([k_array, crossplotz5_array, deltammz5_array]), fmt='%e', delimiter=' ')
+#careful, this does not have the minus sign
 #I got what is needed to plot the ration Delta_{m,psi}/Delta_{m,m}, by modifying previous line
+
+
+
+
+
 
 #****************************************************************************************************************************************
 #Need to interpolate P_m(k,zobs)
@@ -586,99 +627,128 @@ for mm in range(0,16):
 	pcrossplotz5_array[mm] = mid_array[9]
 	mm = mm + 1
 
-np.savetxt('plot1Dprueba_default7z1.dat', np.transpose([k_array, pcrossplotz1_array, pmm_arrayz1]), fmt='%e', delimiter=' ')
-np.savetxt('plot1Dprueba_default7z2.dat', np.transpose([k_array, pcrossplotz2_array, pmm_arrayz2]), fmt='%e', delimiter=' ')
-np.savetxt('plot1Dprueba_default7z3.dat', np.transpose([k_array, pcrossplotz3_array, pmm_arrayz3]), fmt='%e', delimiter=' ')
-np.savetxt('plot1Dprueba_default7z4.dat', np.transpose([k_array, pcrossplotz4_array, pmm_arrayz4]), fmt='%e', delimiter=' ')
-np.savetxt('plot1Dprueba_default7z5.dat', np.transpose([k_array, pcrossplotz5_array, pmm_arrayz5]), fmt='%e', delimiter=' ')
+np.savetxt('plot1Dprueba_modelA1z1.dat', np.transpose([k_array, pcrossplotz1_array, pmm_arrayz1]), fmt='%e', delimiter=' ')
+np.savetxt('plot1Dprueba_modelA1z2.dat', np.transpose([k_array, pcrossplotz2_array, pmm_arrayz2]), fmt='%e', delimiter=' ')
+np.savetxt('plot1Dprueba_modelA1z3.dat', np.transpose([k_array, pcrossplotz3_array, pmm_arrayz3]), fmt='%e', delimiter=' ')
+np.savetxt('plot1Dprueba_modelA1z4.dat', np.transpose([k_array, pcrossplotz4_array, pmm_arrayz4]), fmt='%e', delimiter=' ')
+np.savetxt('plot1Dprueba_modelA1z5.dat', np.transpose([k_array, pcrossplotz5_array, pmm_arrayz5]), fmt='%e', delimiter=' ')
+#again, note that the minus sign from transforming from neutral to ionized is not included in this plots!!!
 
 
 
-#****************************************************************************************************************************************
-#Here we start the process for finding out what happened at the 1D power spectrum, first the linear part. Let's ignore the bias ratio till the comparison is due
-#def linear_integrand_z1(kpar, kper, kmag):
-#	factor = kper*((1 + (kpar**2)/(kmag**2))**2)  
-#	return factor*autopower_z1(kmag)/(2*np.pi)
 
-#def linear_integrand_z2(kpar, kper, kmag):
-#	factor = kper*((1 + (kpar**2)/(kmag**2))**2)  
-#	return factor*autopower_z2(kmag)/(2*np.pi)
 
-#def linear_integrand_z3(kpar, kper, kmag):
-#	factor = kper*((1 + (kpar**2)/(kmag**2))**2)  
-#	return factor*autopower_z3(kmag)/(2*np.pi)
 
-#def linear_integrand_z4(kpar, kper, kmag):
-#	factor = kper*((1 + (kpar**2)/(kmag**2))**2)  
-#	return factor*autopower_z4(kmag)/(2*np.pi)
 
-#def linear_integrand_z5(kpar, kper, kmag):
-#	factor = kper*((1 + (kpar**2)/(kmag**2))**2)  
-#	return factor*autopower_z5(kmag)/(2*np.pi)
 
-#now the new term, again ignoring the bias and factor of 2
-#def new_integrand_z1(kpar, kper, kmag):
-#	factor =  factor = kper*(1 + kpar/kmag)
-#	return factor*crosspower_z1(redshift_test,kmag)*(np.pi/kmag**3)
 
-#def new_integrand_z2(kpar, kper, kmag):
-#	factor =  factor = kper*(1 + kpar/kmag)
-#	return factor*crosspower_z2(redshift_test,kmag)*(np.pi/kmag**3)
 
-#def new_integrand_z3(kpar, kper, kmag):
-#	factor =  factor = kper*(1 + kpar/kmag)
-#	return factor*crosspower_z3(redshift_test,kmag)*(np.pi/kmag**3)
+#***************************************************************************************************************************************
+#ok, here we have to put the 1d observation as the denominator
+#first load and interpolate the stuff
+#note that I took care of units and prepared the format in oned_obs_transform.py
 
-#def new_integrand_z4(kpar, kper, kmag):
-#	factor =  factor = kper*(1 + kpar/kmag)
-#	return factor*crosspower_z4(redshift_test,kmag)*(np.pi/kmag**3)
+#nevermind things are not working we will deal with everything here
 
-#def new_integrand_z5(kpar, kper, kmag):
-#	factor =  factor = kper*(1 + kpar/kmag)
-#	return factor*crosspower_z5(redshift_test,kmag)*(np.pi/kmag**3)
+#important conversion factor from experimental to theory
+def conver_factor(z):
+	return hubble(z)/(1 + z)
 
-#delta_k = 0.01
-#ii = 0
-#lz1 = 0 
-#lz2 = 0
-#lz3 = 0
-#lz4 = 0
-#lz5 = 0
-#sz1 = 0
-#sz2 = 0 
-#sz3 = 0
-#sz4 = 0
-#sz5 = 0
-#k_per = 0
-#let's do this for an specific k parallel
-#k_par = k_test
 
-#for ii in range(0,999):
-#	k_per += 0.01 + delta_k*ii
-#	k_mag = np.sqrt(k_par**2 + k_per**2)
-#	lz1 += linear_integrand_z1(k_par, k_per, k_mag)
-#	lz2 += linear_integrand_z2(k_par, k_per, k_mag)
-#	lz3 += linear_integrand_z3(k_par, k_per, k_mag)
-#	lz4 += linear_integrand_z4(k_par, k_per, k_mag)
-#	lz5 += linear_integrand_z5(k_par, k_per, k_mag)
-#	sz1 += new_integrand_z1(k_par, k_per, k_mag)
-#	sz2 += new_integrand_z2(k_par, k_per, k_mag)
-#	sz3 += new_integrand_z3(k_par, k_per, k_mag)
-#	sz4 += new_integrand_z4(k_par, k_per, k_mag)
-#	sz5 += new_integrand_z5(k_par, k_per, k_mag)
-#	ii = ii + 1
+obs_table = np.loadtxt('oned_lya_obs.dat')
+
+
+
+#35 data points per redshift
+
+z_obs_array = np.zeros(12)
+i = 0
+for i in range(0,12):
+	z_obs_array[i] = 2.2 + 0.2*i
+	i = i + 1
+
+k_obs_array = np.zeros(35)
+p_obs_array = np.zeros(420)
+
+mmm = 0 
+nnn = 0
+for mmm in range(0,12):
+	for nnn in range(0,35):
+		if (mmm == 0):
+			k_obs_array[nnn + 35*mmm] = obs_table[nnn + 35*mmm, 1]
+		p_obs_array[nnn + 35*mmm] = obs_table[nnn + 35*mmm, 2]
+		nnn = nnn + 1
+	mmm = mmm + 1 
+
+#print p_obs_array
+# interpolate here and generate the table, note that the k's are in s/km and the power is in km/s
+lya_oned_obs_units = interp2d(z_obs_array, k_obs_array, p_obs_array, kind='cubic')
+#print lya_oned_obs_units(2.5,k_test/conver_factor(2.5))
+#print lya_oned_obs_units(2.5,k_test/conver_factor(2.5))/conver_factor(2.5)
+#print lya_oned_obs_units(2.5,k_test/conver_factor(2.5))/hubble(2.5)
+
+#print lya_oned_obs_units(2.5,k_test/conver_factor(4.0))
+#print lya_oned_obs_units(2.5,k_test/conver_factor(4.0))/conver_factor(4.0)
+#print lya_oned_obs_units(2.5,k_test/conver_factor(4.0))/hubble(4.0)
 
 #*******************************************************************************************************************************************
+#let's see the comparison with this observational, no redshift the 2.0
 
-#final answers with bias now
-#onedcomp_z1 = bias_ratio_z1*sz1/(lz1*1.0)*(-200)
-#onedcomp_z2 = 2.0*bias_ratio_z2*sz2/(lz2*1.0)*(-1.0*100)
-#onedcomp_z3 = 2.0*bias_ratio_z3*sz3/(lz3*1.0)*(-1.0*100)
-#onedcomp_z4 = 2.0*bias_ratio_z4*sz4/(lz4*1.0)*(-1.0*100)
-#onedcomp_z5 = 2.0*bias_ratio_z5*sz5/(lz5*1.0)*(-1.0*100)
+#cannot use the bias ratio since the observed is directly..., so only gamma bias
 
-#print 'Comparison effect for 1D with bias ratio at zobs = 2.0 and k = %f is %e \n' % (k_test, onedcomp_z1)
-#print 'Comparison effect for 1D with bias ratio at zobs = 2.5 and k = %f is %e \n' % (k_test, onedcomp_z2)
-#print 'Comparison effect for 1D with bias ratio at zobs = 3.0 and k = %f is %e \n' % (k_test, onedcomp_z3)
-#print 'Comparison effect for 1D with bias ratio at zobs = 3.5 and k = %f is %e \n' % (k_test, onedcomp_z4)
-#print 'Comparison effect for 1D with bias ratio at zobs = 4.0 and k = %f is %e \n' % (k_test, onedcomp_z5)
+
+
+onedcomp_obs_z2 = bias_G_z2*onedcomp_array[6]/(lya_oned_obs_units(2.5,k_test/conver_factor(2.5))*1.0/conver_factor(2.5))*(-200)
+onedcomp_obs_z3 = bias_G_z3*onedcomp_array[7]/(lya_oned_obs_units(3.0,k_test/conver_factor(3.0))*1.0/conver_factor(3.0))*(-200)
+onedcomp_obs_z4 = bias_G_z4*onedcomp_array[8]/(lya_oned_obs_units(3.5,k_test/conver_factor(3.5))*1.0/conver_factor(3.5))*(-200)
+onedcomp_obs_z5 = bias_G_z5*onedcomp_array[9]/(lya_oned_obs_units(4.0,k_test/conver_factor(4.0))*1.0/conver_factor(4.0))*(-200)
+
+print '*********************************************************************************************************'
+print 'OBSERVATION HERE:'
+print 'Comparison effect for 1D with bias ratio at zobs = 2.5 and k = %f is %e \n' % (k_test, onedcomp_obs_z2)
+print 'Comparison effect for 1D with bias ratio at zobs = 3.0 and k = %f is %e \n' % (k_test, onedcomp_obs_z3)
+print 'Comparison effect for 1D with bias ratio at zobs = 3.5 and k = %f is %e \n' % (k_test, onedcomp_obs_z4)
+print 'Comparison effect for 1D with bias ratio at zobs = 4.0 and k = %f is %e \n' % (k_test, onedcomp_obs_z5)
+
+
+
+#be careful with those units
+
+#later take care of the redshift of 2.0
+#***********************************************************************************************************
+#now time to plot stuff
+
+#first need a k array that works for everyone!!
+
+#these guys are in Mpc^-1
+k_oned_array = [9.60e-02, 0.0997, 1.2e-01, 1.5e-01, 2.0e-01, 2.5e-01, 3.0e-01, 3.5e-01, 4.0e-01, 4.5e-01, 5.0e-01, 5.5e-01, 6.0e-01, 6.5e-01, 7.0e-01, 7.5e-01, 8.0e-01, 8.5e-01, 9.0e-01, 9.5e-01, 1.0, 1.25]
+
+power_oned_arrayz2 = np.zeros(22)
+power_oned_arrayz3 = np.zeros(22)
+power_oned_arrayz4 = np.zeros(22)
+power_oned_arrayz5 = np.zeros(22)
+power_cross_oned_plotz2_array = np.zeros(22)
+power_cross_oned_plotz3_array = np.zeros(22)
+power_cross_oned_plotz4_array = np.zeros(22)
+power_cross_oned_plotz5_array = np.zeros(22)
+mid_point_array = np.zeros(10)
+p1 = 0
+for p1 in range(0,22):
+	power_oned_arrayz2[p1] = lya_oned_obs_units(2.5,k_oned_array[p1]/conver_factor(2.5))/conver_factor(2.5)
+	power_oned_arrayz3[p1] = lya_oned_obs_units(3.0,k_oned_array[p1]/conver_factor(3.0))/conver_factor(3.0)
+	power_oned_arrayz4[p1] = lya_oned_obs_units(3.5,k_oned_array[p1]/conver_factor(3.5))/conver_factor(3.5)
+	power_oned_arrayz5[p1] = lya_oned_obs_units(4.0,k_oned_array[p1]/conver_factor(4.0))/conver_factor(4.0)
+	mid_point_array = integralisimo(k_oned_array[p1])
+	power_cross_oned_plotz2_array[p1] = mid_point_array[6]
+	power_cross_oned_plotz3_array[p1] = mid_point_array[7]
+	power_cross_oned_plotz4_array[p1] = mid_point_array[8]
+	power_cross_oned_plotz5_array[p1] = mid_point_array[9]
+	p1 = p1 + 1
+
+np.savetxt('plot1D_obs_lya_modelA1z2.dat', np.transpose([k_oned_array, power_cross_oned_plotz2_array, power_oned_arrayz2]), fmt='%e', delimiter=' ')
+np.savetxt('plot1D_obs_lya_modelA1z3.dat', np.transpose([k_oned_array, power_cross_oned_plotz3_array, power_oned_arrayz3]), fmt='%e', delimiter=' ')
+np.savetxt('plot1D_obs_lya_modelA1z4.dat', np.transpose([k_oned_array, power_cross_oned_plotz4_array, power_oned_arrayz4]), fmt='%e', delimiter=' ')
+np.savetxt('plot1D_obs_lya_modelA1z5.dat', np.transpose([k_oned_array, power_cross_oned_plotz5_array, power_oned_arrayz5]), fmt='%e', delimiter=' ')
+#again, note that the minus sign from transforming from neutral to ionized is not included in this plots!!!
+
+
